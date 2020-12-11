1a09d400ca0ed5c540e935c3a49ab1b9203e6c53
Miley Cyrus <62071233+nkhachao@users.noreply.github.com>
2020-12-11 19:39:42 +0700

    Added ability to search with phone number

--- lib/search/searchBackend.dart
+++ lib/search/searchBackend.dart
@@ -19,8 +19,16 @@ Future<List<CustomerInfo>> search(String searchInput) async {
         substitutionValues: {"query": searchInput});
     for (final row in results) {
       List<List<dynamic>> phoneNumber = await connection.query(
-          "SELECT phone FROM Customer_phone WHERE cust_code = @phoneNum",substitutionValues: {"phoneNum": row[2]});
+          "SELECT phone FROM Customer_phone WHERE cust_code = @code",substitutionValues: {"code": row[2]});
       finalResults.add(CustomerInfo(row[0], row[1], phoneNumber[0][0], row[2]));
+    }
+    List<List<dynamic>> phoneNumbers = await connection.query(
+        "SELECT cust_code, phone FROM Customer WHERE phone = @query",
+        substitutionValues: {"query": searchInput});
+    for (final row in phoneNumbers) {
+      results = await connection.query(
+          "SELECT fname, lname, FROM Customer WHERE cust_code = @code",substitutionValues: {"code": row[0]});
+      finalResults.add(CustomerInfo(results[0][0], results[0][1], row[1], row[0]));
     }
   return finalResults;
 }
